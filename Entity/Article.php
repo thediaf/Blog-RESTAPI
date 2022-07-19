be79f1edd90fbb1e47105089c999a4dbd09d8c18
thediaf <jaharacs98@gmail.com>
2022-07-19 16:23:37 +0000

    feat: entity Article

---
+++ Entity/Article.php
@@ -0,0 +1,64 @@
+<?php
+namespace App\Entity;
+
+use DateTime;
+
+class Article
+{
+    protected $id;
+    protected $title;
+    protected $content;
+    protected $createdAt;
+    protected $updatedAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(?int $id)
+    {
+        $this->id = $id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(?string $title)
+    {
+        $this->title = $title;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(?string $content)
+    {
+        $this->content = $content;
+    }
+
+    public function getCreatedAt(): ?DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(?DateTime $createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    public function getUpdatedAt(): ?DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?DateTime $updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+    }
+    
+}
